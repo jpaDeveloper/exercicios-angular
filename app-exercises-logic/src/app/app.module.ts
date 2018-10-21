4e6862bdc435267bdfc364c676e16550c908cb36
jpedro12 <joao.sjc1210@gmail.com>
2018-10-21 17:48:24 -0300

    add component exercises

--- app-exercises-logic/src/app/app.module.ts
+++ app-exercises-logic/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExercisesComponent } from './list-exercises/exercises/exercises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExercisesComponent
   ],
   imports: [
     BrowserModule
