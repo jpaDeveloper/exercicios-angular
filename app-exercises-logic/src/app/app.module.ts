b3844b46a888f10386795cbb1722329d0e44d20d
JOAO PEDRO DE ALMEIDA <joao.almeida@dl.local>
2018-10-23 12:39:18 -0300

    exercise 1 success

--- app-exercises-logic/src/app/app.module.ts
+++ app-exercises-logic/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ExercisesComponent } from './list-exercises/exercises/exercises.component';
@@ -8,9 +9,12 @@ import { ExercisesComponent } from './list-exercises/exercises/exercises.compone
   declarations: [
     AppComponent,
     ExercisesComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app-exercises-logic/src/app/list-exercises/exercises/exercises.component.ts
+++ app-exercises-logic/src/app/list-exercises/exercises/exercises.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-exercises',
   templateUrl: './exercises.component.html',
   styleUrls: ['./exercises.component.css']
 })
 export class ExercisesComponent implements OnInit {
-  cliente = {
-    nome: ""
+  textValue = '';
+  numero = 0.0;
+
+  verificarNumero(value: string): void {  
+    if(value==''){
+    	alert('Digite algum número!');
+    }
+    else{
+    	this.numero = parseFloat(value);
+      if(this.numero == 0){
+        alert('O número é 0!');
+      }
+    	else if((this.numero % 2 == 0) && (this.numero > 0)){
+    		alert('O número ' + this.numero + ' é par e positivo!');
+    	}
+      else if((this.numero % 2 == 0) && (this.numero < 0)){
+        alert('O número ' + this.numero + ' é par e é negativo');
+      }
+      else if(this.numero < 0){
+        alert('O número ' + this.numero +' não é par e é negativo!');
+      }
+    	else{
+    		alert('O número ' + this.numero +' não é par e é positivo!');
+    	}
+    }
+    this.textValue = '';
+    //alert(value);
   }
+
   constructor() { }
 
   ngOnInit() {
