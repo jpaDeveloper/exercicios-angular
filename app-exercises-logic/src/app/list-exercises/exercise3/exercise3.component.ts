ad32b664df9515c1b7eadddc21fd474c783cecbf
JOAO PEDRO DE ALMEIDA <joao.almeida@dl.local>
2018-10-26 15:16:51 -0300

    exercise3 success

--- app-exercises-logic/src/app/list-exercises/exercise3/exercise3.component.ts
+++ app-exercises-logic/src/app/list-exercises/exercise3/exercise3.component.ts
@@ -6,33 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercise3.component.css']
 })
 export class Exercise3Component implements OnInit {
+	tamanho = 0;
 	textValue = '';
-	strIdade = '';
 	intIdade = 0;
+	menor = 0; 
+	maior = 0;
+	media = 0.0;
 	list: number [] = [0];
-	i = 0; //controle
+	i = 0; //controle da lista
   verificarIdades(idade: string): void{
+  	var media = 0.0;
   	this.intIdade = parseInt(idade);
   	if (this.intIdade > 0) {
-  		this.list[this.i] = this.intIdade;
+  		if(this.i == 0){ // Set inicial das variaveis maior e menor
+  			this.menor = this.intIdade;
+  			this.maior = this.intIdade;
+  		}
+  		this.list[this.i] = this.intIdade; // Coloca a idade digitada na lista 
+  		if(this.list[this.i] > this.maior)
+  			this.maior = this.list[this.i];
+  		else if (this.list[this.i] < this.menor)
+  			this.menor = this.list[this.i];
   		this.i++;
-  		alert('Lista: '+ this.list);
+  		alert('Lista: ' + this.list);
   	}
-  	else if (this.intIdade < 0)
+  	else if (this.intIdade < 0) // Solicita idade valida (maior que zero)
   		alert('Por favor digite uma idade válida!');
-  	
-  	// this.intIdade = parseInt(idade);
-  	// if (this.intIdade > 0) {
-  	// 	this.list[this.i] = this.intIdade;
-  	// 	this.strIdade = this.strIdade + ' ' +idade; //concatenando as strings e dividindos por espaco
-  	// 	alert('Idades concatenadas '+this.strIdade);
-  	// }
-  	// else if (this.intIdade < 0)
-  	// 	alert('Por favor digite uma idade válida!');
-  	// else if ((this.intIdade == 0 ) && (this.strIdade == ''))
-  	// 	alert('Sem idade suficiente para calcular uma média. \nPor favor digite ao menos duas idades!');
-  	
-  	this.textValue = '';
+  	else if ((this.intIdade == 0) && (this.list.length >= 2)){ // Verifica se ao digitar 0 para encerrar a lista possui mais de duas idades
+  		for (var i = this.list.length - 1; i >= 0; i--) {// Percorre a lista para salvar as idades e calcular a media
+  			this.media = this.media + this.list[i];
+  		}
+  		this.tamanho = this.list.length; // Pega o tamanho da lista para calcular a media
+  		media = this.media / this.tamanho;
+  		alert('A menor idade é: ' + this.menor + '\nA maior idade é: '+ this.maior + '\nA média é: '+ media);
+  		this.list = []; // Limpa a lista
+  		this.i = 0;
+  		this.menor = 0;
+  		this.maior = 0;
+  		this.media = 0.0;
+  	}
+  	else
+  		alert('Por favor digite ao menos dois valores antes de encerrar.');
+  	this.textValue = ''; //Limpa o input do html
   }
   constructor() { }
 
