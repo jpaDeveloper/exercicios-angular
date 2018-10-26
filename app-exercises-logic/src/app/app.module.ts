6305b7e6dfbf98271e70281298f61d2099bbc19b
JOAO PEDRO DE ALMEIDA <joao.almeida@dl.local>
2018-10-26 16:39:22 -0300

    exercise4 description

--- app-exercises-logic/src/app/app.module.ts
+++ app-exercises-logic/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ExercisesComponent } from './list-exercises/exercises/exercises.component';
 import { Exercise2Component } from './list-exercises/exercise2/exercise2.component';
 import { Exercise3Component } from './list-exercises/exercise3/exercise3.component';
+import { Exercise4Component } from './list-exercises/exercise4/exercise4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExercisesComponent,
     Exercise2Component,
-    Exercise3Component
+    Exercise3Component,
+    Exercise4Component
 
   ],
   imports: [
